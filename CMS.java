06674883529797cfe91b98bb6f8f11dc43c55663
Anmeetkaur <150541850+Anmeetkaur@users.noreply.github.com>
2024-07-11 22:02:38 +0530

    Add files via upload

---
+++ CMS.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cms;
+
+/**
+ *
+ * @author 91904
+ */
+public class CMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrontPage ob = new FrontPage();
+        ob.setVisible(true);
+
+    }
+    
+}
